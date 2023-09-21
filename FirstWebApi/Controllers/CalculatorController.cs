a53b527e818cd78739d6efe4ff5d280162ddc90a
Aravind  L N <aravind.ln@gavstech.com>
2023-09-21 12:04:33 +0530

    First WebApo Commit

---
+++ FirstWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // api/Calculator/Add?x=5&y=10
+        [HttpGet("Calculator/Add")]
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        [HttpGet("Calculator/Sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y+1000;
+        }
+        // api/Calculator/Subtract?x=5&y=10
+        [HttpPost]
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        // api/Calculator/Multiply?x=5&y=10
+        [HttpPut]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        // api/Calculator/Divide?x=5&y=10
+        [HttpDelete]
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
